d04256d02248346d592525457d90fc5f63ebf7df
Tony Spataro <tony@rightscale.com>
2016-05-05 18:08:57 -0700

    Add two more Ruby versions to CI matrix. Move development dependencies from gemspec into Gemfile. Ensure latest greatest bundler during CI.

--- spec/docker/rainbow_spec.rb
+++ spec/docker/rainbow_spec.rb
@@ -4,8 +4,4 @@ describe Docker::Rainbow do
   it 'has a version number' do
     expect(Docker::Rainbow::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
